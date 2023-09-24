1661b5e29bbf72c26cace927c9b1b1b9150bf22e
manisha <72194244+Manisha-404@users.noreply.github.com>
2023-09-24 18:24:16 +0530

    Update App.js
    undo changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Header from './Components/Header';
-import Display from './Components/Display';
 
 function App() {
   return (
     <>
       <Header/>
-      <Display></Display>
     </>
   );
 }
