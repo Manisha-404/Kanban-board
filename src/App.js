92850d3d57458fdbd5b91a1fd37fbbafa3769dc5
manisha <72194244+Manisha-404@users.noreply.github.com>
2023-09-24 18:21:37 +0530

    Update App.js
    card on loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Display from './Components/Display';
 
 function App() {
   return (
     <>
       <Header/>
+      <Display></Display>
     </>
   );
 }
